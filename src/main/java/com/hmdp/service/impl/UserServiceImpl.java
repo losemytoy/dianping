846de4e98fc07651fd32837883eeec924f1627f2
linyiming <2391660155@qq.com>
2024-01-12 11:28:46 +0800

    Initial commit

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,80 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexPatterns;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.sql.Wrapper;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code", code);
+        log.debug("发送短信验证码成功，验证码：{}", code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误");
+        }
+
+        User user = lambdaQuery().eq(User::getPhone, phone).one();
+
+        if (user == null) {
+            user = createUserWithPhone(phone);
+        }
+
+        session.setAttribute("user", BeanUtil.copyProperties(user,UserDTO.class));
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = User.builder()
+                .phone(phone)
+                .nickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10)).build();
+        save(user);
+        return user;
+    }
+}
